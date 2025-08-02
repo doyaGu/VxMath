febe3beccc8eb2ea2c52a050621728111290a0c4
doyaGu <doyaGu@users.noreply.github.com>
2025-08-02 00:31:57 -0400

    Remove friend declarations for InitVxMath and ShutDownVxMath from VxThread class

--- include/VxThread.h
+++ include/VxThread.h
@@ -156,9 +156,6 @@ protected:
     XString m_Name; ///< The name of the thread.
 
 private:
-    friend void InitVxMath();
-    friend void ShutDownVxMath();
-
     /// @brief Private copy constructor to prevent copying.
     VxThread(const VxThread &);
     /// @brief Private assignment operator to prevent assignment.
