1fd918ecafbfb1c870071acfb6ba0b2970541adf
doyaGu <doyaGu@users.noreply.github.com>
2025-11-18 23:43:37 -0500

    Improve type safety in VxMemoryPool aligned allocation

--- src/VxMemoryPool.cpp
+++ src/VxMemoryPool.cpp
@@ -1,9 +1,12 @@
 #include "VxMemoryPool.h"
+#include <cstdint>
 
 void *VxNewAligned(int size, int align) {
     XBYTE *ptr = new XBYTE[size + align + 4];
-    *(XBYTE **) ((~(align - 1) & (*(XDWORD *) &ptr + align + 3)) - 4) = ptr;
-    return (void *) (~(align - 1) & (*(XDWORD *) &ptr + align + 3));
+    uintptr_t addr = reinterpret_cast<uintptr_t>(ptr);
+    uintptr_t aligned = (~static_cast<uintptr_t>(align - 1) & (addr + align + 3));
+    *(reinterpret_cast<XBYTE**>(aligned - 4)) = ptr;
+    return reinterpret_cast<void*>(aligned);
 }
 
 void VxDeleteAligned(void *ptr) {
