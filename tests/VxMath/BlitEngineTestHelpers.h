23341f49157438eb2c1da06cb54e6a3d9c32f625
doyaGu <10153309+doyaGu@users.noreply.github.com>
2026-08-05 00:00:39 -0400

    Isolate STB test diagnostics

--- tests/VxMath/BlitEngineTestHelpers.h
+++ tests/VxMath/BlitEngineTestHelpers.h
@@ -15,8 +15,15 @@
 #include "VxBlitEngine.h"
 #include "XString.h"
 
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#endif
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#endif
 
 #include <gtest/gtest.h>
 #include <vector>
